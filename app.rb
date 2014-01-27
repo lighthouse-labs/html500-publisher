e419c4ea09224a99cd9c8412e01c52290e49988a
Khurram Virani <kvirani@gmail.com>
2014-01-27 00:36:57 -0800

    added sinatra contrib. Moved configure block into setup. Enabled sessions

--- app.rb
+++ app.rb
@@ -1,10 +1,5 @@
 require_relative 'setup'
 
-configure do 
-  set :server, :puma 
-  set :database, ENV['DATABASE_URL'] || "sqlite3:///db/dev.db"
-end
-
 get '/' do
   "Users: #{User.count}"
 end
--- setup.rb
+++ setup.rb
@@ -1,3 +1,5 @@
+## LOAD
+
 if ENV['RACK_ENV'] == 'development'
   require 'dotenv'
   Dotenv.load
@@ -7,3 +9,11 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 require_relative 'models/user'
+
+## CONFIGURE
+
+configure do 
+  set :server, :puma 
+  set :database, ENV['DATABASE_URL'] || "sqlite3:///db/dev.db"
+  enable :sessions
+end
